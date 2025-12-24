570cb5e49eec385fa177bffeb07e34b3e84f22cc
kdongsu5509 <kod66170@gmail.com>
2025-12-24 15:39:30 +0900

    feat(issue10): add spring cloud config and eureka setting

--- imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/ImhereUserServiceApplication.kt
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/ImhereUserServiceApplication.kt
@@ -2,8 +2,10 @@ package com.kdongsu5509.imhereuserservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class ImhereUserServiceApplication
 
 fun main(args: Array<String>) {
