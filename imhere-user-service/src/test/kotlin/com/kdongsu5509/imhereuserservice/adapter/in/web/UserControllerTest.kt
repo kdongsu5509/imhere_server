89b7867982299094f1dd2fff85a4fc95c6b1334f
kdongsu5509 <kod66170@gmail.com>
2026-01-18 14:10:22 +0900

    fix(issue25) : add transaction to test logic
    테스트 간의 고유성을 보장하기 위해 transaction 추가.

--- imhere-user-service/src/test/kotlin/com/kdongsu5509/imhereuserservice/adapter/in/web/UserControllerTest.kt
+++ imhere-user-service/src/test/kotlin/com/kdongsu5509/imhereuserservice/adapter/in/web/UserControllerTest.kt
@@ -17,9 +17,11 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Transactional
 @AutoConfigureMockMvc
 class UserControllerTest {
 
