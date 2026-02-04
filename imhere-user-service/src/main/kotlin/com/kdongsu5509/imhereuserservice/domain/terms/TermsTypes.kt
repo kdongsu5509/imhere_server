f6c02ca66680cda6ab8316b20241fea1a68887c3
kdongsu5509 <kod66170@gmail.com>
2026-02-04 19:16:09 +0900

    feat(issue27) : add new domain class about TERM

---
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/domain/terms/TermsTypes.kt
@@ -0,0 +1,9 @@
+package com.kdongsu5509.imhereuserservice.domain.terms
+
+enum class TermsTypes {
+    SERVICE, //서비스 이용 약관
+    PRIVACY, //개인정보 처리 방침
+    LOCATION, //위치정보 이용약관
+    THIRD_PARTY_SHARING, //개인정보 및 위치정보 제3자 제공 동의
+    MARKETING //마케팅 정보 수신 동의
+}
