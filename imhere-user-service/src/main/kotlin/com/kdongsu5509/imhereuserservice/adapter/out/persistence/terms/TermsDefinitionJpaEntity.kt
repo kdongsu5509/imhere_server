766e0ce9e09ae3c97e524be8da12406c5cf0fb8f
kdongsu5509 <kod66170@gmail.com>
2026-02-04 19:18:20 +0900

    feat(issue27) : add new jpa entity about term

---
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/terms/TermsDefinitionJpaEntity.kt
@@ -0,0 +1,25 @@
+package com.kdongsu5509.imhereuserservice.adapter.out.persistence.terms
+
+import com.kdongsu5509.imhereuserservice.domain.terms.TermsTypes
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "terms")
+class TermsDefinitionJpaEntity(
+    @Column(nullable = false)
+    var termsTitle: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var termsType: TermsTypes,
+
+    @Column(nullable = false)
+    var isRequired: Boolean
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @OneToMany(mappedBy = "terms", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    val versions: MutableList<TermsVersionJpaEntity> = mutableListOf()
+}
