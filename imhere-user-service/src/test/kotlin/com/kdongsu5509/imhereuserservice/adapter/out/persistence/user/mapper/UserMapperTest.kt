945dc560ebceb2edc8f2de9c5f3bd3389387ebe9
kdongsu5509 <kod66170@gmail.com>
2026-02-07 15:32:33 +0900

    fix : change UserMapper.kt and

--- imhere-user-service/src/test/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/user/mapper/UserMapperTest.kt
+++ imhere-user-service/src/test/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/user/mapper/UserMapperTest.kt
@@ -4,23 +4,27 @@ import com.kdongsu5509.imhereuserservice.adapter.out.persistence.user.jpa.UserJp
 import com.kdongsu5509.imhereuserservice.domain.user.OAuth2Provider
 import com.kdongsu5509.imhereuserservice.domain.user.User
 import com.kdongsu5509.imhereuserservice.domain.user.UserRole
+import com.kdongsu5509.imhereuserservice.domain.user.UserStatus
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 class UserMapperTest {
 
     private val userMapper = UserMapper()
 
-    @Test
+    @ParameterizedTest
+    @EnumSource(UserStatus::class)
     @DisplayName("Domain 객체를 JpaEntity로 올바르게 변환해야 한다")
-    fun shouldMapDomainToJpaEntity() {
+    fun shouldMapDomainToJpaEntity(userStatus: UserStatus) {
         // given
         val domainUser = User(
             email = "test@example.com",
             nickname = "동수",
             oauthProvider = OAuth2Provider.KAKAO,
-            role = UserRole.NORMAL
+            role = UserRole.NORMAL,
+            status = userStatus
         )
 
         // when
@@ -31,17 +35,20 @@ class UserMapperTest {
         assertThat(jpaEntity.nickname).isEqualTo(domainUser.nickname)
         assertThat(jpaEntity.role).isEqualTo(domainUser.role)
         assertThat(jpaEntity.provider).isEqualTo(domainUser.oauthProvider)
+        assertThat(jpaEntity.status).isEqualTo(domainUser.status)
     }
 
-    @Test
-    @DisplayName("JpaEntity를 Domain 객체로 올바르게 변환해야 한다")
-    fun shouldMapJpaEntityToDomain() {
+    @ParameterizedTest
+    @EnumSource(UserStatus::class)
+    @DisplayName("기본적인 jpaEntity를 Domain 객체로 올바르게 변환해야 한다")
+    fun shouldMapJpaEntityToDomain(userStatus: UserStatus) {
         // given
         val jpaEntity = UserJpaEntity(
             "test@example.com",
             "동수",
             UserRole.NORMAL,
-            OAuth2Provider.KAKAO
+            OAuth2Provider.KAKAO,
+            userStatus
         )
 
         // when
@@ -52,5 +59,6 @@ class UserMapperTest {
         assertThat(domainUser.nickname).isEqualTo(jpaEntity.nickname)
         assertThat(domainUser.role).isEqualTo(jpaEntity.role)
         assertThat(domainUser.oauthProvider).isEqualTo(jpaEntity.provider)
+        assertThat(domainUser.status).isEqualTo(jpaEntity.status)
     }
 }
