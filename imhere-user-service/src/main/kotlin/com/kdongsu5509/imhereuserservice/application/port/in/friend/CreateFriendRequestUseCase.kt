4295fb615a6493ece7f3a7f0c2901f16d4fd3cfd
kdongsu5509 <kod66170@gmail.com>
2026-02-09 21:30:59 +0900

    feat : change friend request method

--- imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/application/port/in/friend/CreateFriendRequestUseCase.kt
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/application/port/in/friend/CreateFriendRequestUseCase.kt
@@ -1,5 +1,8 @@
 package com.kdongsu5509.imhereuserservice.application.port.`in`.friend
 
+import com.kdongsu5509.imhereuserservice.domain.friend.FriendRequest
+import java.util.*
+
 interface CreateFriendRequestUseCase {
-    fun request(myEmail: String, targetEmail: String)
+    fun request(myEmail: String, receiverId: UUID, message: String): FriendRequest
 }
