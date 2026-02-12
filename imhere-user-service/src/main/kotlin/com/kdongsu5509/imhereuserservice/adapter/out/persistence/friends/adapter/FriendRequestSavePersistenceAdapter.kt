c2bb81b83c26a7fe60a34330ea7b72993d9ba1ed
kdongsu5509 <kod66170@gmail.com>
2026-02-12 23:03:42 +0900

    fix : add verifying logic when user fetching
    userRepository의 query 과정을 2 -> 1 회로 줄이면서 누락된 검증 로직 추가

--- imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/friends/adapter/FriendRequestSavePersistenceAdapter.kt
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/friends/adapter/FriendRequestSavePersistenceAdapter.kt
@@ -4,8 +4,11 @@ import com.kdongsu5509.imhereuserservice.adapter.out.persistence.friends.jpa.Fri
 import com.kdongsu5509.imhereuserservice.adapter.out.persistence.friends.jpa.SpringDataFriendRequestRepository
 import com.kdongsu5509.imhereuserservice.adapter.out.persistence.friends.mapper.FriendRequestMapper
 import com.kdongsu5509.imhereuserservice.adapter.out.persistence.user.jpa.SpringQueryDSLUserRepository
+import com.kdongsu5509.imhereuserservice.adapter.out.persistence.user.jpa.UserJpaEntity
 import com.kdongsu5509.imhereuserservice.application.port.out.friend.FriendRequestSavePort
 import com.kdongsu5509.imhereuserservice.domain.friend.FriendRequest
+import com.kdongsu5509.imhereuserservice.support.exception.BusinessException
+import com.kdongsu5509.imhereuserservice.support.exception.ErrorCode
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -16,7 +19,7 @@ class FriendRequestSavePersistenceAdapter(
     private val springDataFriendRequestRepository: SpringDataFriendRequestRepository,
 ) : FriendRequestSavePort {
     override fun save(requesterEmail: String, receiverId: UUID, message: String): FriendRequest {
-        val queryResult = userRepository.findActiveUsersByEmailAndId(requesterEmail, receiverId)
+        val queryResult = fetchRequiredUsers(requesterEmail, receiverId)
 
         val requester = queryResult.first { it.email == requesterEmail }
         val receiver = queryResult.first { it.id == receiverId }
@@ -29,5 +32,17 @@ class FriendRequestSavePersistenceAdapter(
 
         val commandResult = springDataFriendRequestRepository.save(newFriendRequest)
         return friendRequestMapper.mapToDomainEntity(commandResult)
+    }
+
+    private fun fetchRequiredUsers(
+        requesterEmail: String,
+        receiverId: UUID
+    ): List<UserJpaEntity> {
+        val queryResult = userRepository.findActiveUsersByEmailAndId(requesterEmail, receiverId)
+
+        if (queryResult.size != 2) {
+            throw BusinessException(ErrorCode.USER_NOT_FOUND)
+        }
+        return queryResult
     }
 }
