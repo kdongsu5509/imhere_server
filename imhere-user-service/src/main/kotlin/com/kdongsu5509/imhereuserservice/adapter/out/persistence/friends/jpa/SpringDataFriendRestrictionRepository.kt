d48fecf6ba0b1d852922f4cd051f8bad7fe3b56b
kdongsu5509 <kod66170@gmail.com>
2026-02-15 16:43:15 +0900

    feat : add restriction data loading logic and test logic

--- imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/friends/jpa/SpringDataFriendRestrictionRepository.kt
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/friends/jpa/SpringDataFriendRestrictionRepository.kt
@@ -1,8 +1,18 @@
 package com.kdongsu5509.imhereuserservice.adapter.out.persistence.friends.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface SpringDataFriendRestrictionRepository : JpaRepository<FriendRestrictionJpaEntity, Long> {
+    @Query(
+        """
+        select fr from FriendRestrictionJpaEntity fr 
+        join fetch fr.target 
+        where fr.actor.id = :actorId
+    """
+    )
+    fun findByActorId(actorId: UUID): List<FriendRestrictionJpaEntity>
 }
