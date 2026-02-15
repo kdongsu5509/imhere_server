1794237f09cb22214209dcf935293d29cc18526d
kdongsu5509 <kod66170@gmail.com>
2026-02-15 16:27:22 +0900

    refactor : uniform a private method name
    - 사용자를 이메일 기반으로 찾아오거나, 오류가 발생하는 메서드의 이름을 통일.
    - 차후 변경 시 찾기 용이하도록.

--- imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/friends/adapter/FriendRelationshipLoadPersistenceAdapter.kt
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/out/persistence/friends/adapter/FriendRelationshipLoadPersistenceAdapter.kt
@@ -17,12 +17,12 @@ class FriendRelationshipLoadPersistenceAdapter(
     private val springDataFriendRelationshipsRepository: SpringDataFriendRelationshipsRepository,
 ) : FriendRelationshipLoadPort {
     override fun findFriendsByUserEmail(email: String): List<FriendRelationship> {
-        val userEntity = findUserOrThrow(email)
+        val userEntity = fetchRequiredUser(email)
         val relationshipEntities = springDataFriendRelationshipsRepository.findByOwnerUserId(userEntity.id!!)
         return relationshipEntities.map { friendRelationshipMapper.mapToDomainEntity(it) }
     }
 
-    private fun findUserOrThrow(email: String): UserJpaEntity {
+    private fun fetchRequiredUser(email: String): UserJpaEntity {
         return userRepository.findUserByEmail(email).orElseThrow {
             BusinessException(ErrorCode.USER_NOT_FOUND)
         }
