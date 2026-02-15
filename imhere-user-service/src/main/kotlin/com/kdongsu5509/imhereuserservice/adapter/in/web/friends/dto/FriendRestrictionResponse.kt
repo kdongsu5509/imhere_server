e784197c170bbc53a97401bc139b5c1d4975f19c
kdongsu5509 <kod66170@gmail.com>
2026-02-15 16:42:29 +0900

    fix : add restriction type to domain and repsonse

--- imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/in/web/friends/dto/FriendRestrictionResponse.kt
+++ imhere-user-service/src/main/kotlin/com/kdongsu5509/imhereuserservice/adapter/in/web/friends/dto/FriendRestrictionResponse.kt
@@ -1,12 +1,14 @@
 package com.kdongsu5509.imhereuserservice.adapter.`in`.web.friends.dto
 
 import com.kdongsu5509.imhereuserservice.domain.friend.FriendRestriction
+import com.kdongsu5509.imhereuserservice.domain.friend.FriendRestrictionType
 import java.time.LocalDateTime
 
 data class FriendRestrictionResponse(
     val friendRestrictionId: Long,
     val targetEmail: String,
     val targetNickname: String,
+    val restrictionType: FriendRestrictionType,
     val createdAt: LocalDateTime,
 ) {
     companion object {
@@ -15,6 +17,7 @@ data class FriendRestrictionResponse(
                 domain.friendRestrictionId,
                 domain.targetEmail,
                 domain.targetNickname,
+                domain.restrictionType,
                 domain.createdAt
             )
         }
