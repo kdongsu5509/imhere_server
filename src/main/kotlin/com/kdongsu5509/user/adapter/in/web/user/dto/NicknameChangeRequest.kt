860f621c77b23eee6eda7594926749f9290f2b88
kdongsu5509 <kod66170@gmail.com>
2026-03-20 17:49:16 +0900

    fix : add limitation of nickname size
    솔라피는 문자의 길이에 따라 과금 정보가 다름. 이 때문에 문자 정보에 포함될 닉네임 정보는 최대 5자로 제한하는 것으로 변경

--- src/main/kotlin/com/kdongsu5509/user/adapter/in/web/user/dto/NicknameChangeRequest.kt
+++ src/main/kotlin/com/kdongsu5509/user/adapter/in/web/user/dto/NicknameChangeRequest.kt
@@ -1,8 +1,10 @@
 package com.kdongsu5509.user.adapter.`in`.web.user.dto
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class NicknameChangeRequest(
-    @field:NotBlank(message = "?�로???�네?��? ?�수?�니??")
+    @field:NotBlank(message = "공백은 허용하지 않습니다")
+    @field:Size(max = 5, message = "닉네임은 최대 5글자까지 가능합니다")
     val newNickname: String
 )
