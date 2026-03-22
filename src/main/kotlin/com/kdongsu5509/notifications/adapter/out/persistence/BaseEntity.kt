d23a97f8971eaa8b397bc0b1e281128e534c581b
kdongsu5509 <kod66170@gmail.com>
2026-03-22 15:51:28 +0900

    fix : move base entity position

--- src/main/kotlin/com/kdongsu5509/notifications/adapter/out/persistence/BaseEntity.kt
+++ src/main/kotlin/com/kdongsu5509/notifications/adapter/out/persistence/BaseEntity.kt
@@ -1,4 +1,4 @@
-package com.kdongsu5509.notifications.adapter.out
+package com.kdongsu5509.notifications.adapter.out.persistence
 
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
