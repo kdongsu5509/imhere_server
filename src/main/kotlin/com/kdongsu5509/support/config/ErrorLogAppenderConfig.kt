4239e541bba40eefd1d74d353c5932bb0eba1855
kdongsu5509 <kod66170@gmail.com>
2026-04-11 18:40:56 +0900

    fix: fix configuration error
    존재하지 않는 none 대신 설정 파일에서 설정하지 않음으로써 오류 해결

--- src/main/kotlin/com/kdongsu5509/support/config/ErrorLogAppenderConfig.kt
+++ src/main/kotlin/com/kdongsu5509/support/config/ErrorLogAppenderConfig.kt
@@ -11,8 +11,10 @@ import ch.qos.logback.core.util.FileSize
 import jakarta.annotation.PostConstruct
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("!test")
 class ErrorLogAppenderConfig {
 
     companion object {
