28da998617abc7feb85382278e52a31302497cbf
kdongsu5509 <kod66170@gmail.com>
2026-05-11 20:01:45 +0900

    chore : remove unused logger in KakaoOauthClient
    KakaoOauthClient 클래스에서 사용하지 않는 SLF4J 로거(LoggerFactory) 임포트 및 log 변수 선언을 제거하여 코드를 정리함.

--- src/main/kotlin/com/kdongsu5509/user/adapter/out/auth/oauth/KakaoOauthClient.kt
+++ src/main/kotlin/com/kdongsu5509/user/adapter/out/auth/oauth/KakaoOauthClient.kt
@@ -2,7 +2,6 @@ package com.kdongsu5509.user.adapter.out.auth.oauth
 
 import com.kdongsu5509.user.adapter.out.auth.oauth.dto.OIDCPublicKeyResponse
 import com.kdongsu5509.user.application.port.out.user.oauth.OauthClientPort
-import org.slf4j.LoggerFactory
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 
@@ -10,8 +9,6 @@ import org.springframework.stereotype.Component
 class KakaoOauthClient(
     private val kakaoOauthPublicKeyApiClient: KakaoOauthPublicKeyApiClient
 ) : OauthClientPort {
-
-    private val log = LoggerFactory.getLogger(KakaoOauthClient::class.java)
 
     @Cacheable(value = ["kakaoOidcKeys"], cacheManager = "oidcCacheManager", key = "'kakaoPublicKeySet'")
     override fun fetchPublicKey(): OIDCPublicKeyResponse? {
