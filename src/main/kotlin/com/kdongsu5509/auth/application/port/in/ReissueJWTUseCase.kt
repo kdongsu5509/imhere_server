dc8a0c631b1d5dde0c766b23d3289d95fbbba39b
kdongsu5509 <kod66170@gmail.com>
2026-05-18 21:59:40 +0900

    refactor : re-add refresh feature

--- src/main/kotlin/com/kdongsu5509/auth/application/port/in/ReissueJWTUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.kdongsu5509.auth.application.port.`in`
-
-import com.kdongsu5509.auth.application.ImHereJwtToken
-
-interface ReissueJWTUseCase {
-    fun reissueByRefreshToken(refreshToken: String): ImHereJwtToken
-    fun reissueByUserEmail(email: String): ImHereJwtToken
-}
